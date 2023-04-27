d08fc0ba45c97c165f2181226da155204b3876ff
jaumpelicon <peliconjoao@gmail.com>
2023-04-27 13:48:23 -0400

    [UPDATE] Publish jera_create_feature

--- jera_create_feature/__brick__/{{name.snakeCase()}}/{{#has_factory}}di{{/has_factory}}/{{name.snakeCase()}}_factory.dart
+++ jera_create_feature/__brick__/{{name.snakeCase()}}/{{#has_factory}}di{{/has_factory}}/{{name.snakeCase()}}_factory.dart
@@ -5,7 +5,7 @@ import '../{{name.snakeCase()}}_view_controller.dart';
 import '../{{name.snakeCase()}}_view_model.dart';
 
 class {{name.pascalCase()}}Factory {
-  final String {{name.camelCase()}}Route = '{{name.snakeCase()}}_route';
+  static const String {{name.camelCase()}}Route = '/{{name.snakeCase()}}_route';
 
   static StatefulWidget {{name.camelCase()}}() {
     final viewModel = {{name.pascalCase()}}ViewModel();
