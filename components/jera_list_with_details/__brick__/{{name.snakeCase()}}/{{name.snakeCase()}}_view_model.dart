0f3e9ccec08d49dfb90dc10eb9806a0aca29a005
Joice de Souza Araujo <joice@jera.com.br>
2023-12-19 16:43:08 -0400

    [UPDATE] Merge request fixes

--- components/jera_list_with_details/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_view_model.dart
+++ components/jera_list_with_details/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_view_model.dart
@@ -4,7 +4,7 @@ import '{{name.snakeCase()}}_view_controller.dart';
 
 class {{name.pascalCase()}}ViewModel extends {{name.pascalCase()}}Protocol implements {{name.pascalCase()}}ItemDelegate {
   bool _isEmpty = false;
-  bool _idLoading = false;
+  bool _isLoading = false;
   
   // TODO: Essa é a lista que contém os dados que irá construir o seu componente
   final _data = [
@@ -38,7 +38,7 @@ class {{name.pascalCase()}}ViewModel extends {{name.pascalCase()}}Protocol imple
 
   // TODO: implementar a lógica para controlar os feedbacks de loading e vazio
   @override
-  bool get isLoading => _idLoading;
+  bool get isLoading => _isLoading;
 
   @override
   bool get isEmpty => _isEmpty;
