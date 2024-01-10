865dcd814d3f3ac583e4411079658fe39209aa8a
Danilo-Mota <danilofilomota@gmail.com>
2024-01-10 12:52:54 -0300

    FIX add async

--- utils/jera_database_provider/hooks/post_gen.dart
+++ utils/jera_database_provider/hooks/post_gen.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
+void run(HookContext context) async {
   final progressSQFlite = context.logger.progress('Adicionando o sqflite');
   await Process.run('flutter', ['pub', 'add', 'sqflite'], runInShell: true);
   progressSQFlite.complete();
