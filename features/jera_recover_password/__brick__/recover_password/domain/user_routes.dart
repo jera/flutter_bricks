daefa3645d83611a372996be02e5cbc1973f574c
rodrigogaleano <rodrigo1galeano@gmail.com>
2024-02-05 11:30:32 -0400

    [UPDATE] Add fake api route

---
+++ features/jera_recover_password/__brick__/recover_password/domain/user_routes.dart
@@ -0,0 +1,33 @@
+abstract class UserRoutesProtocol {
+  void recoverPassword({
+    required String email,
+    Success? success,
+    Failure? failure,
+    VoidCallback? onComplete,
+  });
+}
+
+class UserRoutes extends UserRoutesProtocol {
+  final ApiProvider _provider = ServiceLocator.get<ApiProvider>();
+
+  @override
+  void recoverPassword({
+    required String email,
+    Success? success,
+    Failure? failure,
+    VoidCallback? onComplete,
+  }) {
+    final endpoint = Endpoint(
+      path: '/users/recover_password',
+      method: 'POST',
+      data: {'email': email},
+    );
+
+    _provider.request(
+      endpoint: endpoint,
+      success: success,
+      failure: failure,
+      onComplete: onComplete,
+    );
+  }
+}
